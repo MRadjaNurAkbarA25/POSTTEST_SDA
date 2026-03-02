6c33ffabed46c11837aa007df171cf87917ea27d
MRadjaNurAkbarA25 <gulalimerahjambu16@gmail.com>
2026-03-02 16:27:52 +0700

    Finish Post Test 1

--- POSTTEST_1/soal1.cpp
+++ POSTTEST_1/soal1.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 using namespace std;
 
+int FindMin (int arr[], int ukuran, int &indeksMin) {
+    int min = arr[0]; //Flag nilai terkecil
+    indeksMin =0; //Flag indeks dari nilai terkecil
+
+    for (int i = 1; i < ukuran; i++)
+    {
+        if (arr[i] < min) //Perbandingan nilai
+        {
+            min = arr[i]; //Ganti nilai min setiap kali perbandingan true
+            indeksMin = i; //Ganti juga indeksnya
+        }
+    }
+    return min;
+
+}
 
 int main()
 {
     int A[8] = {1, 1, 2, 3, 5, 8, 13, 21}; //Ukuran array dan elemen
-    int min = A[0]; //Flag nilai terkecil
-    int indeksMin =0; //Flag indeks dari nilai terkecil
-
+    int indeksMin = 0;
     /* Analisis Kompleksitas Waktu Berdasarkan Tabel dan Hitung Manual:
         Tmin    = C1 + C2 + C3n + C4n-C4 + 0 + C6n-C6 + C7n + C8 + C9
                 = (C3 + C4 + C6 + C7)n + (C1 + C2 + C8 + C9 – C4 – C6)
@@ -19,19 +32,11 @@ int main()
         Best Case  : Fungsi linear ==> O(n)
         Worst Case : Fungsi linear ==> O(n)
         Best case jika urutan pada array menaik, worst case jika urutan pada array menurun */
-
-    for (int i = 1; i < 8; i++)
-    {
-        if (A[i] < min) //Perbandingan nilai
-        {
-            min = A[i]; //Ganti nilai min setiap kali perbandingan true
-            indeksMin = i; //Ganti juga indeksnya
-        }
-    }
+    int min = FindMin(A, 8, indeksMin);
     cout << "Array : ";
     for (int x : A) cout << x << " ";
 
-    cout << "\n\nNilai mininum : " << min << endl;
-    cout << "Pada indeks : " << indeksMin << endl;
+    cout << "\n\nNilai mininum: " << min << endl;
+    cout << "Pada indeks: " << indeksMin << endl;
     return 0;
 }
